c020bd7186b31ae313544f44fc986190a7458274
MatthiasBandemer <matthias@bandemer.de>
2020-06-14 15:36:58 +0200

    improved restaurants.js with card layout

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -8,21 +8,18 @@ const Restaurants = ({ restaurants }) => {
         {restaurants.map((restaurant, i) => {
             return (
             <Link to={`/restaurant/${restaurant.node.strapiId}`} className="uk-link-reset">
-                <div className="uk-flex-center uk-margin" data-uk-grid>
-                    <div className="uk-width-2-3@m">
-                        <p id="title" className="uk-text-large">
+                <div className="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" data-uk-grid>
+                    <div className="uk-card-media-left uk-cover-container">
+                        <img src={restaurant.node.image.publicURL} alt={restaurant.node.image.publicURL} data-uk-cover/>
+                        <canvas id="picture" width="600" height="400">Picture</canvas>
+                    </div>
+                    <div className="uk-card-body">
+                        <h3 id="title" className="uk-card-title">
                             {restaurant.node.name}
-                        </p>
-                        <p id="description" className="uk-text-small">
+                        </h3>
+                        <p id="description">
                             {restaurant.node.description}
                         </p>
-                    </div>
-                    <div className="uk-width-1-3@m uk-flex-first">
-                        <img
-                        src={restaurant.node.image.publicURL}
-                        alt={restaurant.node.image.publicURL}
-                        width="250"
-                        />
                     </div>
                 </div>
             </Link>)
