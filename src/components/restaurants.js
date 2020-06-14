8abce2c267c91e63f42e9a7678fe954c3030ef9b
MatthiasBandemer <matthias@bandemer.de>
2020-06-14 15:12:08 +0200

    Simplified restaurants.js and removed card.js

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,19 +1,32 @@
 import React from "react"
-import Card from "./card"
+import { Link } from "gatsby"
 
 const Restaurants = ({ restaurants }) => {
 
   return (
     <div>
-      <div className="uk-child-width-1-2" data-uk-grid>
-        <div>
-          {restaurants.map((restaurant, i) => {
+        {restaurants.map((restaurant, i) => {
             return (
-              <Card restaurant={restaurant} key={`restaurant__${restaurant.node.id}`} />
-            )
-          })}
-        </div>
-      </div>
+            <Link to={`/restaurant/${restaurant.node.strapiId}`} className="uk-link-reset">
+                <div className="uk-flex-center uk-margin" data-uk-grid>
+                    <div className="uk-width-2-3@m">
+                        <p id="title" className="uk-text-large">
+                            {restaurant.node.name}
+                        </p>
+                        <p id="description" className="uk-text-small">
+                            {restaurant.node.description}
+                        </p>
+                    </div>
+                    <div className="uk-width-1-3@m uk-flex-first">
+                        <img
+                        src={restaurant.node.image.publicURL}
+                        alt={restaurant.node.image.publicURL}
+                        width="250"
+                        />
+                    </div>
+                </div>
+            </Link>)
+        })}
     </div>
   )
 }
